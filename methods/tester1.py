792d116f6c062efbfa21be015e0f67bde08cb20e
Lamine NDAO <75149635+laminendao@users.noreply.github.com>
2023-11-09 10:52:15 +0100

    update

--- methods/tester1.py
+++ methods/tester1.py
@@ -1,14 +1,4 @@
-from keras.models import Sequential
-from keras.layers import Dense
-
-# Créez un modèle Keras équivalent à votre modèle MLP
-model_keras = Sequential()
-model_keras.add(Dense(100, activation='relu', input_shape=(X_train.shape[1],)))
-model_keras.add(Dense(100, activation='relu'))
-model_keras.add(Dense(1))
-
-# Compilez le modèle Keras
-model_keras.compile(optimizer='adam', loss='mean_squared_error')
-
-# Copiez les poids du modèle MLP dans le modèle Keras
-model_keras.set_weights(model.coefs_)
+import numpy as np
+import shap
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
